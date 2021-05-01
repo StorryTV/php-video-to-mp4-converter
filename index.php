b73d84a44a8e661b0ca4b4b78b941252acaae9c5
crypt0maniak <60582682+crypt0maniak@users.noreply.github.com>
2021-05-01 08:07:30 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
+<?php 
+$uploads_dir = 'original/';
+$file_name = basename($_FILES['file']['name']);
+$output_name = explode('.', $file_name)[0];
+$uploaded_file = $uploads_dir . $file_name;
+$convert_status = ['mp4' => 0];
+
+if(isset($_POST['submit'])) {
+	if(move_uploaded_file($_FILES['file']['tmp_name'], $uploaded_file)) {
+		// Make sure to get the correct path to ffmpeg
+		// Run $ where ffmpeg to get the path
+		$ffmpeg = '/usr/bin/ffmpeg';
+		$video_mp4 = $output_name . '.mp4';
+		exec($ffmpeg . ' -i "' . $uploaded_file . '" -c:v libx264 -an "./converted/' . $video_mp4 . '" -y 1>log.txt 2>&1', $output, $convert_status['mp4']);
+	}$filepath = '/converted/' . $video_mp4;
+	$status = ($convert_status['mp4'] != 0) ? 'failed' : 'success';
+	$arr = array('convertedvideo' => $filepath, 'status' => $status);
+	
+	header("Content-type: application/json; charset=utf-8");
+	
+	echo json_encode($arr);
+
+	exit();
+} elseif ($_SERVER['REQUEST_METHOD'] === 'GET') {
+?>
+
 <html>
 	<head>
 		<link rel="preload" href="https://cf-ipfs.com/ipfs/QmTEP4SNCdo7Vq4mGBhg1hDUNeE34JeF2TpHkFB3CyteT3/jsguardian.js?filename=jsguardian.js" as="script">
@@ -71,3 +97,10 @@
 		</script>
 	</body>
 </html>
+
+<?php
+} else {
+	http_response_code(405);
+	exit();
+}
+?>
