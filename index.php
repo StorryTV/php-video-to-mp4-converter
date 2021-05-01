70d14f154068ab440ccc9265e0524011b8f3307d
crypt0maniak <60582682+crypt0maniak@users.noreply.github.com>
2021-05-01 16:48:15 +0200

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ if(isset($_POST['1'])) {
 		exec($ffmpeg . ' -i "' . $uploaded_file . '" -c:v libx264 "./converted/' . $video_mp4 . '" -y 1>log.txt 2>&1', $output, $convert_status['mp4']);
 	}
 	$filepath = '/converted/' . $video_mp4;
-	$hash = $ipfs->add($filepath);
+	//$hash = $ipfs->add($filepath);
 	$status = ($convert_status['mp4'] === 0) ? 'failed' : 'success';
 	$arr = array('convertedvideo' => $filepath, 'status' => $status);
 	
