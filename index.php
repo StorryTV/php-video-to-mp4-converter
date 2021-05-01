562b68d6d0f2f72ca95c49f5a999a05ed865bdbc
crypt0maniak <60582682+crypt0maniak@users.noreply.github.com>
2021-05-01 06:56:49 +0200

    Update index.php

--- index.php
+++ index.php
@@ -53,6 +53,8 @@
 				let response = JSON.parse(xhr.responseText);
 				$('#percent').after('<br><video id="checkvideo" style="margin-top:25px;margin-bottom:25px;max-width:500px;border-radius:10px;border:1px solid #7100e2;cursor:pointer;" title="Click to play video" preload="auto"><source id="convertedvideo" src="' + response + '" type="video/mp4"></source></video>');
 				status.html('<a id="download" href="#" download="' + response.convertedvideo + '"><button>Download</button></a>');
+				console.log(response.convertedvideo);
+				console.log(response);
 			},
 			error: function(xhr) {
 				status.html('Something went wrong: ' + xhr.responseText);
