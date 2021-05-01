1f2a813b7ae7170922591d1bda3eb43adbe1fcdd
crypt0maniak <60582682+crypt0maniak@users.noreply.github.com>
2021-05-01 02:56:00 +0200

    Update process.php

--- process.php
+++ process.php
@@ -3,20 +3,15 @@ $uploads_dir = 'original/';
 $file_name = basename($_FILES['file']['name']);
 $output_name = explode('.', $file_name)[0];
 $uploaded_file = $uploads_dir . $file_name;
-$convert_status = ['mp4' => 0;
+$convert_status = ['mp4' => 0];
 
 if(isset($_POST['submit'])) {
   if(move_uploaded_file($_FILES['file']['tmp_name'], $uploaded_file)) {
     // Make sure to get the correct path to ffmpeg
     // Run $ where ffmpeg to get the path
     $ffmpeg = '/usr/bin/ffmpeg';
-    
-    // MP4
     $video_mp4 = $output_name . '.mp4';
-    exec($ffmpeg . ' -i "' . $uploaded_file . '" -c:v libx264 -an "./converted/' . $video_mp4 . '" -y 1>convert.txt 2>&1', $output, $convert_status['mp4']);
-
-    // Debug
-    // echo '<pre>' . print_r($output, 1) . ' </pre>';
+    exec($ffmpeg . ' -i "' . $uploaded_file . '" -c:v libx264 -an "./converted/' . $video_mp4 . '" -y 1>log.txt 2>&1', $output, $convert_status['mp4']);
   }
 }
 ?>
@@ -102,7 +97,7 @@ if(isset($_POST['submit'])) {
       </h2>
     </hgroup>
     <video autoplay loop muted poster="">
-      <source src="<?= '/converted/' . $video_mp4; ?>" type="video/mp4">
+      <source src="/converted/<?php echo $video_mp4; ?>" type="video/mp4">
     </video>
   </div>
 </body>
