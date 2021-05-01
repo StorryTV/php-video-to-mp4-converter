d5f4ee15822ebdc4beaf89af72eb1e88b7699c93
crypt0maniak <60582682+crypt0maniak@users.noreply.github.com>
2021-05-01 05:04:14 +0200

    Update convert.php

--- convert.php
+++ convert.php
@@ -14,91 +14,21 @@ if(isset($_POST['submit'])) {
     exec($ffmpeg . ' -i "' . $uploaded_file . '" -c:v libx264 -an "./converted/' . $video_mp4 . '" -y 1>log.txt 2>&1', $output, $convert_status['mp4']);
   }
 }
+
+ob_clean();
+$data = '{"convertedvideo":"/converted/' . $video_mp4 . "}';
+$json = json_encode($data);
+if ($json === false) {
+  $json = json_encode(["jsonError" => json_last_error_msg()]);
+  if ($json === false) {
+    $json = '{"jsonError":"unknown"}';
+  }
+  http_response_code(500);
+}
+  
+header("Content-type: application/json; charset=utf-8");
+echo $json;
+echo ($convert_status['mp4'] != 0) ? '{"status":"failed"}' : '{"status":"success"}';
+
+exit();
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Video to MP4 Converter</title>
-  <style>
-    body {
-      padding: 0;
-      margin: 0;
-      font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
-    }
-
-    .hero {
-      height: 75vh;
-      overflow: hidden;
-      position: relative;
-    }
-
-    .hero video {
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      min-height: 100%;
-      min-width: 100%;
-      z-index: -1;
-      transform: translateY(-50%) translateX(-50%);
-    }
-
-    .hero hgroup {
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      z-index: 1;
-      text-align: center;
-      transform: translateY(-50%) translateX(-50%);
-    }
-
-    .hero h1 {
-      font-size: 60px;
-      margin: 0 0 20px;
-      color: #ffffff;
-      text-shadow: 3px 3px 0 rgba(0,0,0,.5);
-    }
-
-    .hero h2 {
-      font-size: 20px;
-      margin: 0;
-      color: #ffffff;
-      text-shadow: 2px 2px 0 rgba(0,0,0,.5);
-    }
-
-    .fail {
-      background-color: #a50000;
-      display: inline-block;
-      line-height: 1;
-      padding: 5px 7px;
-      border-radius: 5px;
-    }
-
-    .success {
-      background-color: #00a500;
-      display: inline-block;
-      line-height: 1;
-      padding: 5px 7px;
-      border-radius: 5px;
-    }
-  </style>
-</head>
-<body>
-  <div class="hero">
-    <hgroup>
-      <h1><?php echo $output_name ?></h1>
-      <h2>
-        <?php 
-        echo ($convert_status['mp4'] != 0) ? 'MP4: <span class="fail">Fail</span>' : 'MP4: <span class="success">Success</span>';
-        ?>
-      </h2>
-    </hgroup>
-    <video autoplay loop muted poster="">
-      <source src="/converted/<?php echo $video_mp4; ?>" type="video/mp4">
-    </video>
-  </div>
-</body>
-</html>
