57771eecc281dfe2f873142530a05476bdce5bff
StorryTV <60582682+StorryTV@users.noreply.github.com>
2021-05-07 19:38:32 +0200

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,3 +1,4 @@
-currentdir=$(PWD)
+full_path=$(realpath $0)
+currentdir=$(dirname $full_path)
 /usr/bin/find $currentdir/original/ -name "*.mp4" -type f -mtime +1 -exec rm -f {} \;
 /usr/bin/find $currentdir/converted/ -name "*.mp4" -type f -mtime +1 -exec rm -f {} \;
