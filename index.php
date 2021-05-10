3b4cf0fafc4ddb437c72d317926c2e86347eb6a9
StorryTV <60582682+StorryTV@users.noreply.github.com>
2021-05-10 14:14:04 +0200

    Update index.php

--- index.php
+++ index.php
@@ -40,7 +40,8 @@ if(isset($_POST['upload_form'])) {
 		$ffmpeg = '/usr/bin/ffmpeg';
 		$video_mp4 = $output_name . '.mp4';
 		$status = 'converting';
-		exec('echo "' . $status . '" > "./converted/' . $video_mp4 . '".json && ' . $ffmpeg . ' -i "' . $uploaded_file . '" -preset ultrafast -c:v libx264 -c:a aac "./converted/' . $video_mp4 . '" -y 1>log.txt 2>&1 && echo "success" > "./converted/' . $video_mp4 . '".json', $output, $convert_status['mp4']);
+		$arr = array('status' => $status);
+		exec('echo "' . json_encode($arr) . '" > "./converted/' . $video_mp4 . '".json && ' . $ffmpeg . ' -i "' . $uploaded_file . '" -preset ultrafast -c:v libx264 -c:a aac "./converted/' . $video_mp4 . '" -y 1>log.txt 2>&1 && echo "success" > "./converted/' . $video_mp4 . '".json', $output, $convert_status['mp4']);
 	}
 	unlink($_filepath);
 	$filepath = '/converted/' . $video_mp4;
@@ -102,7 +103,7 @@ if(isset($_POST['upload_form'])) {
 					status.empty();
 					let percentVal = '0%';
 					bar.width(percentVal);
-					percent.html(percentVal);
+					percent.html('<p>' + percentVal + '</p>');
 					if ($('#checkvideo')) {
 						$('#checkvideo').remove();
 					}
@@ -117,7 +118,7 @@ if(isset($_POST['upload_form'])) {
 						percentVal = '<svg width="30" height="10" viewBox="0 0 120 30" xmlns="http://www.w3.org/2000/svg" fill="#7100e2"><circle cx="15" cy="15" r="15"><animate attributeName="r" from="15" to="15" begin="0s" dur="0.8s" values="15;9;15" calcMode="linear" repeatCount="indefinite" /><animate attributeName="fill-opacity" from="1" to="1" begin="0s" dur="0.8s" values="1;.5;1" calcMode="linear" repeatCount="indefinite" /></circle><circle cx="60" cy="15" r="9" fill-opacity="0.3"><animate attributeName="r" from="9" to="9" begin="0s" dur="0.8s" values="9;15;9" calcMode="linear" repeatCount="indefinite" /><animate attributeName="fill-opacity" from="0.5" to="0.5" begin="0s" dur="0.8s" values=".5;1;.5" calcMode="linear" repeatCount="indefinite" /></circle><circle cx="105" cy="15" r="15"><animate attributeName="r" from="15" to="15" begin="0s" dur="0.8s" values="15;9;15" calcMode="linear" repeatCount="indefinite" /><animate attributeName="fill-opacity" from="1" to="1" begin="0s" dur="0.8s" values="1;.5;1" calcMode="linear" repeatCount="indefinite" /></circle></svg><p>Processing Video</p>';
 						bar.width('100%');
 					}
-					percent.html(percentVal);
+					percent.html('<p>' + percentVal + '</p>');
 				},
 				complete: function(xhr) {
 					let response = JSON.parse(xhr.responseText);
@@ -130,10 +131,10 @@ if(isset($_POST['upload_form'])) {
 				},
 				error: function(xhr) {
 					let response = JSON.parse(xhr.responseText);
-					(function() {
+					//(function() {
 						(function checkstatus() {
 							$.get('/converted/<?php echo $video_mp4; ?>.json', function(data) {
-								if (data == 'converting') {
+								if (JSON.parse(data.status) == 'converting') {
 									setTimeout(checkstatus, 5000);
 								} else if (data == 'success') {
 									$('#percent').css('display', 'none');
@@ -143,7 +144,7 @@ if(isset($_POST['upload_form'])) {
 								}
 							});
 						})();
-					})();
+					//})();
 				}
 			});
 			
