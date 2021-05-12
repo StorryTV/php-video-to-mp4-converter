eb0ad6a426f09406fc6cc99375aac99e40154b84
StorryTV <60582682+StorryTV@users.noreply.github.com>
2021-05-12 13:24:07 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@ full_path=$(realpath $0)
 currentdir=$(dirname $full_path)
 sudo apt install ffmpeg -y
 sudo chmod -R 775 $currentdir/original $currentdir/converted
+sudo chmod 770 $currentdir/cleanup.php
+sudo chmod 750 $currentdir/install.sh
 sudo chmod 750 $currentdir/cleanup.sh
+sudo chmod 750 $currentdir/log.txt
 crontab -l | { cat; echo "0       *       *       *       *       $currentdir/cleanup.php"; } | crontab -
 echo "Installation done!"
