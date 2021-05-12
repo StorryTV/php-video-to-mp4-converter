145293346fa1698eb74bde6945eb16e1ded00729
StorryTV <60582682+StorryTV@users.noreply.github.com>
2021-05-12 16:04:44 +0200

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -2,5 +2,4 @@
 
 full_path=$(realpath $0)
 currentdir=$(dirname $full_path)
-/usr/bin/find $currentdir/original/ -name "*.mp4" -type f -mtime +1 -exec rm -f {} \;
-/usr/bin/find $currentdir/converted/ -name "*.mp4" -type f -mtime +1 -exec rm -f {} \;
+php $currentdir/cleanup.php
