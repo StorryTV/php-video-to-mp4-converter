3a33103e0e98ff45046481c8892045ab6dc546e7
StorryTV <60582682+StorryTV@users.noreply.github.com>
2021-05-12 13:05:31 +0200

    Create cleanup.php

---
+++ cleanup.php
@@ -0,0 +1,19 @@
+<?php
+
+/** define the directory **/
+$dir = "./original/";
+$dir2 = "./converted/";
+
+/*** cycle through all files in the directory ***/
+foreach (glob($dir."*.*") as $file) {
+
+	/*** if file is 24 hours (86400 seconds) old then delete it ***/
+	if(time() - filemtime($file) > 86400){
+		if ($file != '.*') {
+			echo 'deleting: ' . $file . "\n";
+			unlink($file);
+		}
+	}
+}
+
+?>
