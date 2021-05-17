7e24a4ed874cfc4837ea9e216d192e073d7d6a99
StorryTV <60582682+StorryTV@users.noreply.github.com>
2021-05-18 00:12:44 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,5 @@ sudo apt install ffmpeg -y
 sudo chmod -R 775 $currentdir/original $currentdir/converted
 sudo chmod 770 $currentdir/cleanup.php
 sudo chmod 750 $currentdir/install.sh
-sudo chmod 750 $currentdir/cleanup.sh
 sudo chmod 750 $currentdir/log.txt
 echo "Installation done!"
